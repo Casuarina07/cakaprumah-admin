ce4f0f3e0c9b25eb37472cff49af0b3750b91022
Casuarina07 <casuarina070798@gmail.com>
2022-02-01 22:12:27 +0800

    connection to firebase

---
+++ functions/main.js
@@ -0,0 +1,13 @@
+const config = {
+  apiKey: "AIzaSyCEVsUFIsT74A_dTl8UaCMyJmxBMtRDwyY",
+  authDomain: "freelance-trial.firebaseapp.com",
+  projectId: "freelance-trial",
+  storageBucket: "freelance-trial.appspot.com",
+  messagingSenderId: "333533410713",
+  appId: "1:333533410713:web:0553e41275ed7d78a10f08",
+  measurementId: "G-YLPPNTZZTC",
+};
+
+firebase.initializeApp(config);
+
+const firestore = firebase.firestore();
